b25e3648ee1daccd45b50760ed4f9eae7f0b069c
LnPedro <pedroabraao1611@gmail.com>
2026-05-04 22:31:52 -0300

    database + parcelable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     id("kotlin-parcelize")
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -12,7 +14,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.juliana.login"
+        applicationId = "com.mobile.agregarural"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -49,10 +51,21 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.11.0")
+
+
+    implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+
 }
