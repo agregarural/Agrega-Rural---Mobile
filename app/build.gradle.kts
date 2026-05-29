8541f5f3b29e95ec82f0ecb1636c37efeeca644c
ezssoagui <ezssoagui@gmail.com>
2026-05-28 22:53:26 -0300

    adicionando e mudando foto e nome da pagina perfil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
 
     implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
